ba983f5475b08eee99ad674044620649740a6900
Chris Desir <christopher.desir01@gmail.com>
2021-11-15 22:10:12 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from db import *
+from flask import Flask, request
+import json
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+db_filename = 'tbd.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % db_filename
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+def failure_response(error, code=404):
+    return json.dumps({'success': False, 'error': error}), code
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
